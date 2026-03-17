d31f9d89c18685837d5e4403cfa007009aefc460
yihyun1 <mooneehyeon@gmail.com>
2026-03-17 23:30:01 +0900

    Rename shopping-list.html to index.html

--- test-shopping.mjs
+++ test-shopping.mjs
@@ -3,7 +3,7 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
-const filePath = 'file:///' + path.join(__dirname, 'shopping-list.html').replace(/\\/g, '/');
+const filePath = 'file:///' + path.join(__dirname, 'index.html').replace(/\\/g, '/');
 
 let passed = 0;
 let failed = 0;
